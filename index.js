842b9c1779ed77d7ea1967096fee0f00c6b299e0
zhangzimeng <zhangzimeng01@baidu.com>
2017-05-11 17:26:37 +0800

    add fecs

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 
 /**
+ * @file index
+ * @author zhangzimeng01
+ */
+
+/**
  * Copyright (c) 2015-present, Facebook, Inc.
  * All rights reserved.
  *
@@ -56,216 +61,219 @@ var semver = require('semver');
  */
 var argv = require('minimist')(process.argv.slice(2));
 
-var CLI_MODULE_PATH = function() {
-  return path.resolve(
-    process.cwd(),
-    'node_modules',
-    'html-native',
-    'cli.js'
-  );
+var cliModulePath = function () {
+    return path.resolve(
+        process.cwd(),
+        'node_modules',
+        'html-native',
+        'cli.js'
+    );
 };
 
-var REACT_NATIVE_PACKAGE_JSON_PATH = function() {
-  return path.resolve(
-    process.cwd(),
-    'node_modules',
-    'html-native',
-    'package.json'
-  );
+var reactNativePackageJSONPath = function () {
+    return path.resolve(
+        process.cwd(),
+        'node_modules',
+        'html-native',
+        'package.json'
+    );
 };
 
 checkForVersionArgument();
 
 var cli;
-var cliPath = CLI_MODULE_PATH();
+var cliPath = cliModulePath();
 if (fs.existsSync(cliPath)) {
-  cli = require(cliPath);
+    cli = require(cliPath);
 }
 
 // minimist api
 var commands = argv._;
 if (cli) {
-  cli.run();
+    cli.run();
 } else {
-  if (commands.length === 0) {
-    console.error(
-      'You did not pass any commands, did you mean to run `html-native init`?'
-    );
-    process.exit(1);
-  }
-
-  switch (commands[0]) {
-  case 'init':
-    if (!commands[1]) {
-      console.error(
-        'Usage: html-native init <ProjectName> [--verbose]'
-      );
-      process.exit(1);
-    } else {
-      if (!argv.verbose) console.log('This may take some time...');
-      init(commands[1], argv.verbose, argv.version);
+    if (commands.length === 0) {
+        console.error(
+            'You did not pass any commands, did you mean to run `html-native init`?'
+        );
+        process.exit(1);
     }
-    break;
-  default:
-    console.error(
-      'Command `%s` unrecognized. ' +
-      'Make sure that you have run `npm install` and that you are inside a html-native project.',
-      commands[0]
-    );
-    process.exit(1);
-    break;
-  }
+
+    switch (commands[0]) {
+        case 'init':
+            if (!commands[1]) {
+                console.error(
+                    'Usage: html-native init <ProjectName> [--verbose]'
+                );
+                process.exit(1);
+            } else {
+                if (!argv.verbose) {
+                    console.log('This may take some time...');
+                }
+                init(commands[1], argv.verbose, argv.version);
+            }
+            break;
+        default:
+            console.error(
+                'Command `%s` unrecognized. '
+                + 'Make sure that you have run `npm install` and that you are inside a html-native project.',
+                commands[0]
+            );
+            process.exit(1);
+            break;
+    }
 }
 
 function validatePackageName(name) {
-  if (!name.match(/^[$A-Z_][0-9A-Z_$]*$/i)) {
-    console.error(
-      '"%s" is not a valid name for a project. Please use a valid identifier ' +
-        'name (alphanumeric).',
-      name
-    );
-    process.exit(1);
-  }
+    if (!name.match(/^[$A-Z_][0-9A-Z_$]*$/i)) {
+        console.error(
+            '"%s" is not a valid name for a project. Please use a valid identifier '
+            + 'name (alphanumeric).',
+            name
+        );
+        process.exit(1);
+    }
 
-  if (name === 'React') {
-    console.error(
-      '"%s" is not a valid name for a project. Please do not use the ' +
-        'reserved word "React".',
-      name
-    );
-    process.exit(1);
-  }
+    if (name === 'React') {
+        console.error(
+            '"%s" is not a valid name for a project. Please do not use the '
+            + 'reserved word "React".',
+            name
+        );
+        process.exit(1);
+    }
 }
 
 function init(name, verbose, rnPackage) {
-  validatePackageName(name);
+    validatePackageName(name);
 
-  if (fs.existsSync(name)) {
-    createAfterConfirmation(name, verbose, rnPackage);
-  } else {
-    createProject(name, verbose, rnPackage);
-  }
+    if (fs.existsSync(name)) {
+        createAfterConfirmation(name, verbose, rnPackage);
+    } else {
+        createProject(name, verbose, rnPackage);
+    }
 }
 
 function createAfterConfirmation(name, verbose, rnPackage) {
-  prompt.start();
+    prompt.start();
 
-  var property = {
-    name: 'yesno',
-    message: 'Directory ' + name + ' already exists. Continue?',
-    validator: /y[es]*|n[o]?/,
-    warning: 'Must respond yes or no',
-    default: 'no'
-  };
+    var property = {
+        name: 'yesno',
+        message: 'Directory ' + name + ' already exists. Continue?',
+        validator: /y[es]*|n[o]?/,
+        warning: 'Must respond yes or no',
+        default: 'no'
+    };
 
-  prompt.get(property, function (err, result) {
-    if (result.yesno[0] === 'y') {
-      createProject(name, verbose, rnPackage);
-    } else {
-      console.log('Project initialization canceled');
-      process.exit();
-    }
-  });
+    prompt.get(property, function (err, result) {
+        if (result.yesno[0] === 'y') {
+            createProject(name, verbose, rnPackage);
+        } else {
+            console.log('Project initialization canceled');
+            process.exit();
+        }
+    });
 }
 
 function createProject(name, verbose, rnPackage) {
-  var root = path.resolve(name);
-  var projectName = path.basename(root);
+    var root = path.resolve(name);
+    var projectName = path.basename(root);
 
-  console.log(
-    'This will walk you through creating a new html Native project in',
-    root
-  );
+    console.log(
+        'This will walk you through creating a new html Native project in',
+        root
+    );
 
-  if (!fs.existsSync(root)) {
-    fs.mkdirSync(root);
-  }
-
-  var packageJson = {
-    name: projectName,
-    version: '0.0.1',
-    private: true,
-    scripts: {
-      start: 'node node_modules/html-native/local-cli/cli.js start'
+    if (!fs.existsSync(root)) {
+        fs.mkdirSync(root);
     }
-  };
-  fs.writeFileSync(path.join(root, 'package.json'), JSON.stringify(packageJson));
-  process.chdir(root);
 
-  console.log('Installing html-native package from npm...');
+    var packageJson = {
+        name: projectName,
+        version: '0.0.1',
+        private: true,
+        scripts: {
+            start: 'node node_modules/html-native/local-cli/cli.js start'
+        }
+    };
+    fs.writeFileSync(path.join(root, 'package.json'), JSON.stringify(packageJson));
+    process.chdir(root);
 
-  if (verbose) {
-    runVerbose(root, projectName, rnPackage);
-  } else {
-    run(root, projectName, rnPackage);
-  }
+    console.log('Installing html-native package from npm...');
+
+    if (verbose) {
+        runVerbose(root, projectName, rnPackage);
+    } else {
+        run(root, projectName, rnPackage);
+    }
 }
 
 function getInstallPackage(rnPackage) {
-  var packageToInstall = 'html-native';
-  var valideSemver = semver.valid(rnPackage);
-  if (valideSemver) {
-    packageToInstall += '@' + valideSemver;
-  } else if (rnPackage) {
-    // for tar.gz or alternative paths
-    packageToInstall = rnPackage;
-  }
-  return packageToInstall;
+    var packageToInstall = 'html-native';
+    var valideSemver = semver.valid(rnPackage);
+    if (valideSemver) {
+        packageToInstall += '@' + valideSemver;
+    } else if (rnPackage) {
+        // for tar.gz or alternative paths
+        packageToInstall = rnPackage;
+    }
+    return packageToInstall;
 }
 
 function run(root, projectName, rnPackage) {
-  exec('npm install --save --save-exact ' + getInstallPackage(rnPackage), function(e, stdout, stderr) {
-    if (e) {
-      console.log(stdout);
-      console.error(stderr);
-      console.error('`npm install --save --save-exact html-native` failed');
-      process.exit(1);
-    }
+    exec('npm install --save --save-exact ' + getInstallPackage(rnPackage), function (e, stdout, stderr) {
+        if (e) {
+            console.log(stdout);
+            console.error(stderr);
+            console.error('`npm install --save --save-exact html-native` failed');
+            process.exit(1);
+        }
 
-    checkNodeVersion();
+        checkNodeVersion();
 
-    cli = require(CLI_MODULE_PATH());
-    cli.init(root, projectName);
-  });
+        cli = require(cliModulePath());
+        cli.init(root, projectName);
+    });
 }
 
 function runVerbose(root, projectName, rnPackage) {
-  var proc = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['install', '--verbose', '--save', '--save-exact', getInstallPackage(rnPackage)], {stdio: 'inherit'});
-  proc.on('close', function (code) {
-    if (code !== 0) {
-      console.error('`npm install --save --save-exact html-native` failed');
-      return;
-    }
+    var proc = spawn(/^win/.test(process.platform) ? 'npm.cmd'
+        : 'npm', ['install', '--verbose', '--save', '--save-exact', getInstallPackage(rnPackage)], {stdio: 'inherit'});
+    proc.on('close', function (code) {
+        if (code !== 0) {
+            console.error('`npm install --save --save-exact html-native` failed');
+            return;
+        }
 
-    cli = require(CLI_MODULE_PATH());
-    cli.init(root, projectName);
-  });
+        cli = require(cliModulePath());
+        cli.init(root, projectName);
+    });
 }
 
 function checkNodeVersion() {
-  var packageJson = require(REACT_NATIVE_PACKAGE_JSON_PATH());
-  if (!packageJson.engines || !packageJson.engines.node) {
-    return;
-  }
-  if (!semver.satisfies(process.version, packageJson.engines.node)) {
-    console.error(chalk.red(
-        'You are currently running Node %s but React Native requires %s. ' +
-        'Please use a supported version of Node.\n' +
-        'See https://facebook.github.io/react-native/docs/getting-started.html'
-      ),
-      process.version,
-      packageJson.engines.node);
-  }
+    var packageJson = require(reactNativePackageJSONPath());
+    if (!packageJson.engines || !packageJson.engines.node) {
+        return;
+    }
+    if (!semver.satisfies(process.version, packageJson.engines.node)) {
+        console.error(chalk.red(
+                'You are currently running Node %s but React Native requires %s. '
+                + 'Please use a supported version of Node.\n'
+                + 'See https://facebook.github.io/react-native/docs/getting-started.html'
+            ),
+            process.version,
+            packageJson.engines.node);
+    }
 }
 
 function checkForVersionArgument() {
-  if (argv._.length === 0 && (argv.v || argv.version)) {
-    console.log('html-native-cli: ' + require('./package.json').version);
-    try {
-      console.log('html-native: ' + require(REACT_NATIVE_PACKAGE_JSON_PATH()).version);
-    } catch (e) {
-      console.log('react-native: n/a - not inside a React Native project directory');
+    if (argv._.length === 0 && (argv.v || argv.version)) {
+        console.log('html-native-cli: ' + require('./package.json').version);
+        try {
+            console.log('html-native: ' + require(reactNativePackageJSONPath()).version);
+        } catch (e) {
+            console.log('react-native: n/a - not inside a React Native project directory');
+        }
+        process.exit();
     }
-    process.exit();
-  }
 }
